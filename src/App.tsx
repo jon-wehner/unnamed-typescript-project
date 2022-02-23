2d1271aaa536c7fb45fefc64ff0d6ad68d7007de
Jon Wehner <jonjwehner@gmail.com>
2022-02-22 21:41:18 -0800

    union types

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 interface AppProps {
@@ -6,11 +6,41 @@ interface AppProps {
   extraText?: string,
 }
 
+interface address {
+  street: string,
+  number: number,
+  zip:string,
+}
+interface User {
+  name: string,
+  age: number,
+  country: string,
+  address: address,
+  admin: boolean
+}
+
 function App({ headerText, extraText }: AppProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchUser = () => setUser({
+    name: 'Bob',
+    age: 23,
+    country: 'usa',
+    address: {
+      street: 'Main St',
+      number: 88,
+      zip: '12345',
+    },
+    admin: false,
+  });
+
   return (
     <>
       <h1>{headerText}</h1>
       {extraText && <p>{extraText}</p>}
+
+      <button type="button" onClick={fetchUser}>Fetch User</button>
+      {user && <p>{`hello ${user.name}`}</p>}
     </>
   );
 }
