a50fc4b3df74560e7421f4bf0601e8e4d1794669
Jon Wehner <jonjwehner@gmail.com>
2022-02-22 22:50:24 -0800

    form stuff

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,29 @@ interface AppProps {
   extraText?: string,
 }
 
+const defaultFormData = {
+  title: '',
+  body: '',
+};
+
 function App({ headerText, extraText }: AppProps) {
   const [user, setUser] = useState<User | null>(null);
+  const [formData, setFormData] = useState(defaultFormData);
+  const { title, body } = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+
+    setFormData(defaultFormData);
+  };
 
   const fetchUser = () => setUser({
     name: 'Bob',
@@ -29,6 +50,23 @@ function App({ headerText, extraText }: AppProps) {
 
       <button type="button" onClick={fetchUser}>Fetch User</button>
       {user && <p>{`hello ${user.name}`}</p>}
+
+      <h1>Form</h1>
+      <p>Create a post</p>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">
+          Title
+          <input type="text" id="title" value={title} onChange={onChange} />
+        </label>
+        <br />
+        <label htmlFor="body">
+          Body
+          <input type="text" id="body" name="body" value={body} onChange={onChange} />
+        </label>
+        <br />
+        <button type="submit">Upload post</button>
+      </form>
     </>
   );
 }
